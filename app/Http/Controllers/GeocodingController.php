7006c5ccf6ef9d1f4f304f0f5d2fe156c33382f0
JeftaMukti <115094745+JeftaMukti@users.noreply.github.com>
2023-12-10 22:33:18 +0700

    adding marker and route
    adding marker in ip location and add route from ip location to address

--- app/Http/Controllers/GeocodingController.php
+++ app/Http/Controllers/GeocodingController.php
@@ -8,41 +8,46 @@ use OpenCage\Geocoder\Geocoder;
 class GeocodingController extends Controller
 {
     public function index(Request $request)
-{
-    if ($request->isMethod('post')) {
-        // Handle POST request for geocoding
+    {
+        if ($request->isMethod('post')) {
+            // Handle POST request for geocoding
 
-        // Get address from request
-        $address = $request->input('address');
+            // Get address from request
+            $address = $request->input('address');
 
-        if ($address) {
-            // Geocode address
-            $apiKey = 'a202888050824cf8b394fdd7e6997477';
-            $geocoder = new Geocoder($apiKey);
+            if ($address) {
+                try {
+                    // Geocode address
+                    $apiKey = 'a202888050824cf8b394fdd7e6997477';
+                    $geocoder = new Geocoder($apiKey);
 
-            $result = $geocoder->geocode($address);
+                    $result = $geocoder->geocode($address);
 
-            if ($result['status']['message'] == 'OK') {
-                $data = [
-                    'address' => $address,
-                    'latitude' => $result['results'][0]['geometry']['lat'],
-                    'longitude' => $result['results'][0]['geometry']['lng'],
-                    'formatted_address' => $result['results'][0]['formatted'],
-                ];
+                    if ($result && $result['status']['message'] == 'OK') {
+                        $data = [
+                            'address' => $address,
+                            'latitude' => $result['results'][0]['geometry']['lat'],
+                            'longitude' => $result['results'][0]['geometry']['lng'],
+                            'formatted_address' => $result['results'][0]['formatted'],
+                        ];
 
-                return view('geocoding.index', compact('data'));
+                        return view('geocoding.index', compact('data'));
+                    } else {
+                        // Handle geocoding error
+                        return redirect()->back()->withErrors(['message' => 'Geocoding failed.']);
+                    }
+                } catch (\Exception $e) {
+                    // Catch any other generic exceptions
+                    return redirect()->back()->withErrors(['message' => 'Error occurred during geocoding.']);
+                }
             } else {
-                // Handle geocoding error
-                return redirect()->back()->withErrors(['message' => 'Geocoding failed.']);
+                // Address is missing in POST request
+                return redirect()->back()->withErrors(['message' => 'Please enter an address to geocode.']);
             }
         } else {
-            // Address is missing in POST request
-            return redirect()->back()->withErrors(['message' => 'Please enter an address to geocode.']);
-        }
-    } else {
-        // Handle GET request for displaying the form
+            // Handle GET request for displaying the form
 
-        return view('geocoding.index');
+            return view('geocoding.index');
+        }
     }
-}
 }
--- resources/views/geocoding/index.blade.php
+++ resources/views/geocoding/index.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OpenCage Geocoding</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <!-- Add Leaflet Routing Machine CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
+
     <style>
         #map-container {
             height: 400px;
@@ -14,28 +17,65 @@
 <body>
     <h1>OpenCage Geocoding Example</h1>
 
-        <h2>Geocoding results for {{ $data['address'] }}:</h2>
-        <div id="map-container"></div>
+    @if(isset($data))
+        @if(isset($data['address']))
+            <h2>Geocoding results for {{ $data['address'] }}:</h2>
+            <div id="map-container"></div>
 
-        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+            <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+            <!-- Add Leaflet Routing Machine script -->
+            <script src="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.js"></script>
 
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                var map = L.map('map-container').setView([{{ $data['latitude'] }}, {{ $data['longitude'] }}], 15);
+            <script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    var map = L.map('map-container').setView([{{ $data['latitude'] }}, {{ $data['longitude'] }}], 15);
 
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
+                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    }).addTo(map);
 
-                L.marker([{{ $data['latitude'] }}, {{ $data['longitude'] }}]).addTo(map);
-            });
-        </script>
-        <br>
-        <form method="post" action="{{ route('geocode.index') }}">
-            @csrf
-            <label for="address">Enter address to geocode:</label>
-            <input type="text" name="address" id="address" required>
-            <button type="submit">Geocode</button>
-        </form>
+                    L.marker([{{ $data['latitude'] }}, {{ $data['longitude'] }}]).addTo(map);
+
+                    // Get user's IP-based location
+                    fetch('https://ipapi.co/json/')
+                        .then(response => response.json())
+                        .then(data => {
+                            var userLatitude = parseFloat(data.latitude);
+                            var userLongitude = parseFloat(data.longitude);
+
+                            L.marker([userLatitude, userLongitude]).addTo(map);
+
+                            // Add routing between IP location and searched address
+                            var control = L.Routing.control({
+                                waypoints: [
+                                    L.latLng(userLatitude, userLongitude), // User's IP location
+                                    L.latLng({{ $data['latitude'] }}, {{ $data['longitude'] }}) // Searched address location
+                                ],
+                                routeWhileDragging: true
+                            }).addTo(map);
+                        })
+                        .catch(error => console.log('Error fetching IP-based location:', error));
+                });
+            </script>
+        @endif
+    @endif
+
+    <br>
+    <form method="post" action="{{ route('geocode.index') }}">
+        @csrf
+        <label for="address">Enter address to geocode:</label>
+        <input type="text" name="address" id="address" required>
+        <button type="submit">Geocode</button>
+    </form>
+    
+    @if($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </body>
 </html>
